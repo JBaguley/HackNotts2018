cb5543f022eba204f92bde644bc7434128178ecf
Joe Baguley <joe.baguley@outlook.com>
2018-11-24 21:38:19 +0000

    Getting there

--- api.py
+++ api.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
 import base64
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/api/create", methods=["POST"])
@@ -9,4 +11,4 @@ def receiveData():
     with open("test.jpg", "wb") as f:
         print(request.json)
         f.write(base64.b64decode(request.json["image-data"]))
-    return "Got it!"
+    return request.json["image-data"].encode()
---
+++ script.js
@@ -0,0 +1,44 @@
+function sendData(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            if (this.responseText != "error"){
+                console.log(this.responseText);
+                document.getElementById('createImg').setAttribute(
+                    'src', "data:image/jpeg;base64, "+this.responseText
+                );
+            } else {
+                document.getElementById("demo").innerHTML = "Try again!";
+            }
+        
+        }
+    };
+    xhttp.open("POST", "http://127.0.0.1:5000/api/create", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    sendString = '{\n   '
+    sendString += '"first-name": "';
+    sendString += document.getElementById("first_name").value + "$$";
+    sendString += '",\n    "last-name": "';
+    sendString += document.getElementById("last_name").value + "$$";
+    sendString += '",\n    "email": "';
+    sendString += document.getElementById("email").value + "$$";
+    sendString += '",\n    "date-of-birth": "';
+    sendString += document.getElementById("date_of_birth").value + "$$$";
+    
+    //console.log(sendString);
+    
+    var file = document.getElementById('image').files[0];
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+        var b64long = reader.result;
+        var b64 = b64long.substring(b64long.indexOf(",")+1,b64long.length);
+        //console.log(b64);
+        sendString += '",\n    "image-data": "';
+        sendString += b64;
+        sendString += '"\n}';
+        xhttp.send(sendString);
+      };
+};
+
+ 
